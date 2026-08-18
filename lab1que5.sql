f567b464f32ca41096eb5829be684a8525f0ee6a
badalvishwkarma91-prog <badalvishwkarma91@gmail.com>
2026-08-18 15:16:49 +0530

    lab1que5.sql

---
+++ lab1que5.sql
@@ -0,0 +1,22 @@
+mysql> use badaldbms;
+Database changed
+mysql> create table countries (country_id varchar(2),country_name varchar(40),region_id Decimal(10,0),primary key(country_id));
+Query OK, 0 rows affected (0.19 sec)
+
+mysql> show tables;
++--------------------+
+| Tables_in_badaldbms |
++--------------------+
+| countries          |
++--------------------+
+1 row in set (0.01 sec)
+
+mysql> desc countries;
++--------------+---------------+------+-----+---------+-------+
+| Field        | Type          | Null | Key | Default | Extra |
++--------------+---------------+------+-----+---------+-------+
+| country_id   | varchar(2)    | NO   | PRI | NULL    |       |
+| country_name | varchar(40)   | YES  |     | NULL    |       |
+| region_id    | decimal(10,0) | YES  |     | NULL    |       |
++--------------+---------------+------+-----+---------+-------+
+3 rows in set (0.02 sec)
