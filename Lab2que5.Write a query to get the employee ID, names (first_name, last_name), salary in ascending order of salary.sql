b08ad30a10c1a7fa8339864d739aa02fb71b7cdc
Badal Vishwkarma <badalvishwkarma91@gmail.com>
2026-09-17 23:30:18 +0530

    Add employee database and query by salary
    Created a database and table for employees, inserted sample data, and queried employee details sorted by salary.

---
+++ Lab2que5.Write a query to get the employee ID, names (first_name, last_name), salary in ascending order of salary.sql
@@ -0,0 +1,39 @@
+mysql> create database badaldbms5;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> use badaldbms5;
+Database changed
+
+mysql> create table employees (
+    -> employee_id int,
+    -> first_name varchar(30),
+    -> last_name varchar(30),
+    -> salary decimal(10,2)
+    -> );
+Query OK, 0 rows affected (0.05 sec)
+
+mysql> insert into employees(employee_id,first_name,last_name,salary)
+    -> values
+    -> (101,'Ajay','Yadav',30000),
+    -> (102,'Arun','Tiwari',40000),
+    -> (103,'Avni','Pathak',35000),
+    -> (104,'Badal','Vishwakarma',50000),
+    -> (105,'Laxmi','Patidar',45000);
+Query OK, 5 rows affected (0.01 sec)
+Records: 5  Duplicates: 0  Warnings: 0
+
+mysql> select employee_id, first_name, last_name, salary
+    -> from employees
+    -> order by salary asc;
++-------------+------------+-------------+----------+
+| employee_id | first_name | last_name   | salary   |
++-------------+------------+-------------+----------+
+|         101 | Ajay       | Yadav       | 30000.00 |
+|         103 | Avni       | Pathak      | 35000.00 |
+|         102 | Arun       | Tiwari      | 40000.00 |
+|         105 | Laxmi      | Patidar     | 45000.00 |
+|         104 | Badal      | Vishwakarma | 50000.00 |
++-------------+------------+-------------+----------+
+5 rows in set (0.00 sec)
+
+mysql>
