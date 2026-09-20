af653fc6418224b5076f5234046bcfc04aa7778f
Badal Vishwkarma <badalvishwkarma91@gmail.com>
2026-09-20 21:51:10 +0530

    Add SQL script for calculation of 171*214+625
    Created a SQL script to calculate the expression 171*214+625 and store the result in a database.

---
+++ Lab2que13.Write a query to calculate 171*214+625.sql
@@ -0,0 +1,15 @@
+mysql> create database badaldbms13;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> use badaldbms13;
+Database changed
+
+mysql> select 171*214+625 as `Result`;
++--------+
+| Result |
++--------+
+|  37219 |
++--------+
+1 row in set (0.00 sec)
+
+mysql>
